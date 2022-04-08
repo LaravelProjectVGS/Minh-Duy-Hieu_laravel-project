5ba9de1c8b7a6833e0ad5090425e70a9851c855b
congminh <minh260519988@gmail.com>
2022-04-08 15:54:56 +0700

    fix loi cua Hieu

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-{{-- <div class="phantrang">
-    <ul>
-        <li class="active-phantrang">
-            <a href="#">1</a>
-        </li>
-        <li>
-            <a href="#">2</a>
-        </li>
-        <li>
-            <a href="#">3</a>
-        </li>
-    </ul>
-</div> --}}
-
-
-
-@if ($paginator->hasPages())
-    <nav>
-        <div class="phantrang">
-            <ul>
-                {{-- Previous Page Link --}}
-                {{-- @if ($paginator->onFirstPage())
-                    <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                        <span aria-hidden="true">&lsaquo;</span>
-                    </li>
-                @else
-                    <li>
-                        <a href="{{ $paginator->previousPageUrl() }}" rel="prev"
-                            aria-label="@lang('pagination.previous')">&lsaquo;</a>
-                    </li>
-                @endif --}}
-
-                {{-- Pagination Elements --}}
-                @foreach ($elements as $element)
-                    {{-- "Three Dots" Separator --}}
-                    @if (is_string($element))
-                        <li class="active-phantrang" aria-disabled="true"><span>{{ $element }}</span></li>
-                    @endif
-
-                    {{-- Array Of Links --}}
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <li class="active-phantrang" aria-current="page"><span>{{ $page }}</span></li>
-                            @else
-                                <li><a href="{{ $url }}">{{ $page }}</a></li>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
-
-                {{-- Next Page Link --}}
-                {{-- @if ($paginator->hasMorePages())
-                    <li>
-                        <a href="{{ $paginator->nextPageUrl() }}" rel="next"
-                            aria-label="@lang('pagination.next')">&rsaquo;</a>
-                    </li>
-                @else
-                    <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                        <span aria-hidden="true">&rsaquo;</span>
-                    </li>
-                @endif --}}
-            </ul>
-        </div>
-=======
-=======
->>>>>>> c44b20eb9f39654a5840cb848c0b6714451eb7df
 @if ($paginator->hasPages())
     <nav>
         <ul class="pagination">
@@ -76,7 +7,7 @@
                     <span aria-hidden="true">&lsaquo;</span>
                 </li>
             @else
-                <li>
+                <li  class="asd">
                     <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
                 </li>
             @endif
@@ -111,9 +42,5 @@
                 </li>
             @endif
         </ul>
-<<<<<<< HEAD
->>>>>>> c44b20eb9f39654a5840cb848c0b6714451eb7df
-=======
->>>>>>> c44b20eb9f39654a5840cb848c0b6714451eb7df
     </nav>
 @endif
